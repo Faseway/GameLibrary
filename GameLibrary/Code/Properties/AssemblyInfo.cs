9279ec6b71dd7f1ce81d74e4d3651fff7a168528
MadnessFreak <supervivo@icloud.com>
2015-03-04 11:08:49 +0100

    Add Visual Studio Project

---
+++ GameLibrary/Code/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("Game Library")]
+[assembly: AssemblyDescription("Open source game library for indie games.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Faseway")]
+[assembly: AssemblyProduct("GameLibrary")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 Faseway")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("6e7eecc6-5c83-45bf-801d-374fdb305e4f")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("0.304.*")]
+[assembly: AssemblyFileVersion("0.304.0.0")]
