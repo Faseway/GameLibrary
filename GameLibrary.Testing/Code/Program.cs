e8b08e9066b3a9e5d54bf6a70d7376f4baabfe37
MadnessFreak <supervivo@icloud.com>
2015-03-04 11:51:04 +0100

    Add test project

---
+++ GameLibrary.Testing/Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Faseway.GameLibrary;
+
+namespace Faseway.GameLibrary.Testing
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Library Test App";
+
+            Console.WriteLine("Game Library Test App");
+            Console.WriteLine();
+
+            Seed.Initialize();
+
+            Console.Read();
+        }
+    }
+}
