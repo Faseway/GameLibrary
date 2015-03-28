9e3594f6b63fe903a8b876ca625bbf9bd37bb550
MadnessFreak <supervivo@icloud.com>
2015-03-28 13:59:47 +0100

    Add word wrap extension

---
+++ GameLibrary/Code/Rendering/Font/Extension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Faseway.GameLibrary.Rendering
+{
+    public static class Extension
+    {
+        /// <summary>
+        /// Wraps a specified text.
+        /// </summary>
+        /// <param name="font">The font.</param>
+        /// <param name="text">The text to wrap.</param>
+        /// <param name="length">The length.</param>
+        /// <returns>A wrapped string.</returns>
+        public static string Wrap(this SpriteFont font, string text, int length)
+        {
+            var line = string.Empty;
+            var wrapped = string.Empty;
+
+            foreach (var word in text.Split(' '))
+            {
+                if (font.MeasureString(line + word).Length() > length)
+                {
+                    wrapped = wrapped + line + '\n';
+                    line = string.Empty;
+                }
+
+                line = line + word + ' ';
+            }
+
+            return wrapped + line;
+        }
+    }
+}
